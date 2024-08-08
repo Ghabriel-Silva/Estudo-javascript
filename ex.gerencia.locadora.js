2b6a925fd95640e4a111ef1ccd1c6acbaf41edbd
Gabriel silva <132311565+Ghabriel-Silva@users.noreply.github.com>
2024-08-08 19:37:33 -0300

    Objeto js

--- ex.gerencia.locadora.js
+++ ex.gerencia.locadora.js
@@ -19,3 +19,82 @@ AtualizarPreco: Atualiza o preço de aluguel de um filme.
 ExibirInventario: Exibe todos os filmes no inventário.
  */
 
+const locadora ={
+    Nome: 'Locadora Marechal',
+    Endereco: 'Rua manaus 25, SP',
+    Inventario: [],
+
+
+    //Adiciona um objeto  a meu array Inventario
+    adicionaFilmes: function(titulo, diretor, ano, quantidade, preco){
+        const  novoItem = {
+            titulodoFilme: titulo,
+            DiretordoFilme:diretor,
+            anodoFilme: ano ,
+            quantidadedoFilme: quantidade,
+            precodoFilme:preco,
+        }
+        
+        this.Inventario.push(novoItem)
+    }, 
+    //Remover livros do estoqque 
+    removerFilme: function(nomedofilme){
+        this.Inventario = this.Inventario.filter(item => item.titulodoFilme !== nomedofilme)
+    }, 
+
+    //Pesquisa de disponibilidade em estoque 
+    disponibilidade: function(nomedoFilmeapesquisar) {
+        const item = this.Inventario.find(item => item.titulodoFilme === nomedoFilmeapesquisar);
+        
+        if (item) {
+            if (item.quantidadedoFilme === 0) {
+                console.log('Filme indisponível');
+            } else {
+                console.log(`Filme disponível. Estoque = ${item.quantidadedoFilme}`);
+            }
+        } else {
+            console.log('Filme não encontrado');
+        }
+    },
+
+
+    atualizarProduto: function(nomedofilme, quantfilme, precfilm) {
+        const item = this.Inventario.find(item => item.titulodoFilme === nomedofilme);
+        
+        if (item) {
+            item.quantidadedoFilme = quantfilme; 
+            item.precodoFilme = precfilm;
+        } else {
+            console.log('Filme não encontrado para atualização');
+        }
+    },
+
+    
+    exibirInventario: function(){
+        this.Inventario.forEach(item =>{
+            console.log(`Nome do filme: ${item.titulodoFilme},Ano de lançamento: ${item.anodoFilme}, Diretor do filme: ${item.DiretordoFilme}, Quantidade em Estoque: ${item.quantidadedoFilme}, Preço: R$ ${item.precodoFilme.toFixed(2)}`)
+        } )
+    }
+
+
+
+
+}
+
+//Adicionando filmes 
+locadora.adicionaFilmes('O Senhor dos Anéis', 'Peter Jackson', 2001, 0, 9.99);
+locadora.adicionaFilmes('Matrix', 'Wachowskis', 1999, 20, 7.99);
+locadora.adicionaFilmes('Star Wars', 'George Lucas', 1977,11, 12.99);
+locadora.adicionaFilmes('Avatar', 'James Cameron', 2009,0, 14.99);
+locadora.adicionaFilmes('ketlin', ' Cameron', 2020, 10 , 14.99)
+
+//atualizar produto
+//locadora.atualizarProduto('ketlin',0,100.00)
+
+
+
+//locadora.removerFilme('ketlin')
+
+//locadora.disponibilidade('ketlin')
+
+locadora.exibirInventario()
